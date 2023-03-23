6859a4010b27bf57f015a2ea1050d52a4d00ff2b
dzming-git <dzm_work@163.com>
2023-03-24 01:15:06 +0800

    规范化消息处理回调函数的设置

--- dbot/dbot.py
+++ dbot/dbot.py
@@ -1,18 +1,22 @@
 from dzmicro import DzMicro
-from dbot.dbot_server import creat_server
+from dbot.dbot_server import ServerCreator
 from dbot.dbot_sender import send_message_to_cqhttp
+from dzmicro.app.message_handler.message_handler import message_handler_thread
 import time
+from typing import Callable, List
 
 class DBot(DzMicro):
     def __init__(self):
         super().__init__()
         self._dbot_server = None
+        self.message_handler: Callable[[str, List], None] = message_handler_thread.message_handler
         self.set_send_message_to_source(send_message_to_cqhttp)
     
     def start(self):
         if self.start_server():
             if self.is_platform():
-                self._dbot_server = creat_server(handle_func=self.handle)
+                ServerCreator.set_message_handler(self.message_handler)
+                self._dbot_server = ServerCreator.creat_server()
                 self._dbot_server.serve_forever()
         time.sleep(100)
     
--- dbot/dbot_server.py
+++ dbot/dbot_server.py
@@ -1,24 +1,33 @@
 from flask import Flask, request
 from werkzeug.serving import make_server
+from typing import Callable, List
 
-def creat_server(handle_func: callable):
-    app = Flask(__name__)
-    @app.route('/', methods=["POST"])
-    def post_data():
-        '''
-        将信息源的信息转发至服务程序
-        '''
-        # 获取消息体
-        data = request.get_json()
-        print(data)
-        # 获取source_id
-        qid = data.get('sender', {}).get('user_id')
-        gid = data.get('group_id')
-        source_id = [gid, qid]
-        # 获取原始消息内容
-        message = data.get('raw_message')
-        handle_func(message, source_id)
-        # 返回响应
-        return 'OK'
-    server = make_server(host='127.0.0.1', port=5701, app=app)
-    return server
+class ServerCreator:
+    _message_handler: Callable[[str, List], None] = None
+
+    @classmethod
+    def set_message_handler(cls, message_handler: Callable[[str, List], None]):
+        cls._message_handler = message_handler
+
+    @classmethod
+    def creat_server(cls):
+        app = Flask(__name__)
+        @app.route('/', methods=["POST"])
+        def post_data():
+            '''
+            将信息源的信息转发至服务程序
+            '''
+            # 获取消息体
+            data = request.get_json()
+            print(data)
+            # 获取source_id
+            qid = data.get('sender', {}).get('user_id')
+            gid = data.get('group_id')
+            source_id = [gid, qid]
+            # 获取原始消息内容
+            message = data.get('raw_message')
+            cls._message_handler(message, source_id)
+            # 返回响应
+            return 'OK'
+        server = make_server(host='127.0.0.1', port=5701, app=app)
+        return server
